13ba2b0e0af60744b9668882818180e64cf84dad
milanzagorac <zagoracm@gmail.com>
2020-11-08 19:55:57 -0500

    adding files to repo WIP

---
+++ giphy.js
@@ -0,0 +1,30 @@
+// listen for a click on button with id getGifs
+// get the value of the text in the form
+// create new LI, set the properties of the LI based on the form value
+// 
+
+async function getGifs() {
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log(res);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const btnOne = document.querySelector('#getGifs');
+btnOne.addEventListener('click', getGifs);
+
+const btnTwo = document.querySelector('#remove');
+btnTwo.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    // get the id of the LIs
+    // remove all LI
+})
