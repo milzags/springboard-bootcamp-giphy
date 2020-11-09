153025918eb5611b17235bbf393613c13fabe7d8
milanzagorac <zagoracm@gmail.com>
2020-11-08 20:02:02 -0500

    updated code

--- giphy.js
+++ giphy.js
@@ -4,11 +4,11 @@
 // 
 
 async function getGifs() {
-    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search`, { params: 
+        { q: search, api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym' } });
+
     console.log(res);
 }
-
-
 
 
 
