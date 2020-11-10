17c0a27dd3b205518738335435b6c0fb68ac253b
milanzagorac <zagoracm@gmail.com>
2020-11-10 08:58:29 -0500

    more WIP

---
+++ gif.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    // get element where gifs will be posted:
+    let gifsLocation = document.getElementById('#gifs')
+    // get search input
+    let formInput = document.getElementById('#inputvalue');
+
+    function addGif(response) {
+        //loop through the search term results and pick a gif at random
+        //create a new div element, create a new img element
+        //append the new div to the UL and then append the img to the div
+        let numResults = response.data.length;
+        if (numResults > 0) {
+            let randInd = Math.floor(Math.random() * numResults);
+            let newDiv = document.createElement('div');
+            let newGif = document.createElement('img');
+            newGif.setAttribute('src', res.data[randInd].images.original.url);
+            newDiv.append(gifsLocation);
+            newGif.append(newDiv);
+        }
+    }
+
+    let theForm = document.querySelector('form');
+
+    theForm.addEventListener('click', async function(event) {
+        event.preventDefault();
+        let searchTerm = formInput.value;
+        formInput.value = '';
+
+        const response = await axios.get('http://api.giphy.com/v1/gifs/search', { 
+            params: {
+                q: searchTerm,
+                api_key : "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+            }
+        });
+        addGif(response.data);
+    })
+
+
+});
--- giphy.js
+++ giphy.js
@@ -3,16 +3,23 @@
 // create new LI, set the properties of the LI based on the form value
 // 
 
+// getElement where gif will be placed
 const ul = document.getElementById('#gifs');
-const searchForm = document.getElementById('#description');
+// get element where the search term is entered
+const searchForm = document.getElementById('#inputvalue');
 
-async function getGifs(search) {
+if (searchForm) {
+searchForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    console.log(searchForm.value)
     const res = await axios.get(`http://api.giphy.com/v1/gifs/search`, { params: 
-        { q: searchTerm, api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym' } });
+        { q: search, api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym' } });
         //pass argument to function via value in the form    
-    
+        //to make a request for /resource?a=1&b=2 {params {a: 1, b: 2}}
     console.log(res);
+    });
 }
+
 
 function addGif(res) {
     //get number of results (array)
@@ -25,12 +32,11 @@ function addGif(res) {
 
 
 
-
-
-
-
 const btnOne = document.querySelector('#getGifs');
-btnOne.addEventListener('click', getGifs);
+btnOne.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
 
 const btnTwo = document.querySelector('#remove');
 btnTwo.addEventListener('click', (ev) => {
