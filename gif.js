5864c25cd15e69896a5b2d5e63598eee5f3930b4
milanzagorac <zagoracm@gmail.com>
2020-11-12 20:32:55 -0500

    wip

--- gif.js
+++ gif.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
+document.addEventListener('DOMContentLoaded', (ev) => {
+    ev.preventDefault();
     // get element where gifs will be posted:
     let gifsLocation = document.getElementById('gifs')
     // get search input
@@ -10,12 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
         //create a new div element, create a new img element
         //append the new div to the UL and then append the img to the div
         let numResults = response.data.length;
+
         if (numResults > 0) {
             let randInd = Math.floor(Math.random() * numResults);
-            let newDiv = document.createElement('div');
-            let newGif = document.createElement('img');
+            //create a new div element to add to the DOM in the 
+            //div with #gifs id
+            let newDiv = document.createElement('DIV');
+            //create a new gif image element
+            let newGif = document.createElement('IMG');
+            //set the source of the new IMG element, from the API
             newGif.setAttribute('src', response.data[randInd].images.original.url);
+            // add the new div to the gifs container
             newDiv.append(gifsLocation);
+            // add the new gif to the newDiv
             newGif.append(newDiv);
         }
     }
@@ -45,4 +52,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 });
+
 
